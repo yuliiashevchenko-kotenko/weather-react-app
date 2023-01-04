85c7e9b93232283b0e7abc0730e4714212ace3df
Yuliia Shevchenko-Kotenko <juliashk1990@gmail.com>
2023-01-04 13:36:56 -0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <a href="https://github.com/yuliiashevchenko-kotenko/weather-react-app">
+        Code by
+      </a>
+      Yuliia Shevchenko-Kotenko
     </div>
   );
 }
